e52b92896ce5372fe5aad47d9792e096b85bb576
Nick Castermans <nickcastermans@mbp-van-nick.int.ucll.be>
2017-11-21 16:55:47 +0100

    sign in => works create user => doesn't work

--- TaskManager/app/src/main/java/com/example/nickc/taskmanager/TaskActivity.java
+++ TaskManager/app/src/main/java/com/example/nickc/taskmanager/TaskActivity.java
@@ -8,13 +8,10 @@ import android.widget.Button;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class TaskActivity extends AppCompatActivity {
-
-    private Button add;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
